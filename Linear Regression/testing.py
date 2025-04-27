dcb111de645b99a002a18a202b3f56ee469a0298
sarthak <sarthak@satsure.co>
2025-04-27 17:41:14 +0530

    testing code resolve

--- Linear Regression/testing.py
+++ Linear Regression/testing.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn import datasets 
 import matplotlib.pyplot as plt
 from Linear_Regression import LinearRegression 
+import numpy as np 
 
 ''' Testing of the above build Linear Regression Model'''
 
